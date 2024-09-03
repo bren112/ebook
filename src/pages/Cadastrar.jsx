398d4ef8f5f163be5bbe4a89f3bc2b55fa9f026e
brenin <brenomarinho1610@gmail.com>
2024-09-03 14:53:14 -0300

    Btn Login - PAGE:CADASTRAR

--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -4,6 +4,8 @@ import Snackbar from '@mui/material/Snackbar';
 import { supabase } from './supabaseclient'; 
 import InputMask from 'react-input-mask'; 
 import './cadastrar.css'; 
+import { Link } from "react-router-dom";
+
 
 function Cadastrar() {
   const [nome, setNome] = useState('');
@@ -53,7 +55,9 @@ function Cadastrar() {
       <br />
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label>Escolha seu avatar:</label>
+          <label id='avt'>Escolha seu avatar <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+</svg></label>
           <div className="avatar-buttons">
             <div className="central">
               <button
@@ -133,6 +137,10 @@ function Cadastrar() {
           <button type="submit" disabled={loading}>
             {loading ? 'Enviando...' : 'Cadastrar'}
           </button>
+
+          <Link to="/login">
+            <button id="button">Login!</button>
+          </Link>
         </div>
       </form>
 
