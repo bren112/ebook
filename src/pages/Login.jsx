f32918c5cb60daa4f157154fe4c479f5b0faf93f
brenin <brenomarinho1610@gmail.com>
2024-09-05 11:40:54 -0300

    Esqueceu a Senha

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { supabase } from './supabaseclient'; 
 import './cadastrar.css'; 
 import './ebook.css';  
-import parse from 'html-react-parser';  // Importe o parser HTML
+import parse from 'html-react-parser';  
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
 import Dialog from '@mui/material/Dialog';
@@ -22,21 +22,22 @@ function Login() {
   const [openDialog, setOpenDialog] = useState(false);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
-  const [loading, setLoading] = useState(true); // estado de carregamento
+  const [loading, setLoading] = useState(true);
+  const [readingMode, setReadingMode] = useState(false); // Estado do modo leitura
 
   // Recarregar login do localStorage
   useEffect(() => {
     const storedData = localStorage.getItem('userData');
     if (storedData) {
       setUserData(JSON.parse(storedData));
-      fetchArticles(); // Buscar artigos se o usuário já estiver logado
+      fetchArticles(); 
     }
   }, []);
 
   const fetchArticles = async () => {
     try {
       const { data, error } = await supabase
-        .from('Artigos') // nome da tabela
+        .from('Artigos')
         .select('*');
 
       if (error) {
@@ -53,20 +54,18 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setMessage(''); 
+    setMessage('');
 
-    // Buscar o usuário com o email fornecido
     const { data, error } = await supabase
       .from('users')
       .select('email, senha, nome, avatar') 
-      .eq('email', email) // filtragem
+      .eq('email', email)
       .single();
 
     if (error) {
       setMessage('Erro ao buscar o email: ' + error.message);
     } else if (data) {
-      // Se o email for encontrado, verifica a senha
-      if (data.senha === password) { // Comparar a senha fornecida com a senha armazenada
+      if (data.senha === password) {
         const user = {
           nome: data.nome,
           avatar: data.avatar,
@@ -74,7 +73,7 @@ function Login() {
 
         setUserData(user);
         localStorage.setItem('userData', JSON.stringify(user));
-        fetchArticles(); // Buscar artigos após o login bem-sucedido
+        fetchArticles();
       } else {
         setMessage('Senha incorreta.');
       }
@@ -100,20 +99,24 @@ function Login() {
     setSnackbarOpen(false);
   };
 
+  // Função para alternar entre o modo leitura
+  const toggleReadingMode = () => {
+    setReadingMode((prevMode) => !prevMode);
+  };
+
   return (
-    <div className="login-container">
+    <div className={`login-container ${readingMode ? 'reading-mode' : ''}`}>
       <br />
       {userData ? (
         <div className="container">
           <div className="user_logado">
             <div className="user_info">
-            {userData.avatar ? (
+              {userData.avatar ? (
                 <img src={userData.avatar} alt="Avatar" width="100" height="100" />
               ) : (
                 <p>Sem avatar</p>
               )}
               <h2>Bem-Vindo, <br /><span id='user_name'>{userData.nome}!</span></h2>
-        
             </div>
             <button id='sair' onClick={handleLogout}>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
@@ -126,14 +129,17 @@ function Login() {
           <h1 id='titulo-ebook'>E-BOOKS</h1>
           <br />
 
+          <button onClick={toggleReadingMode} className="toggle-reading-mode">
+            {readingMode ? 'Desativar Modo Leitura' : 'Ativar Modo Leitura'}
+          </button>
+
           {loading ? (
-            <div className="loader"></div> // Círculo giratório enquanto carrega
+            <div className="loader"></div>
           ) : (
             <div className="articles-container">
               {articles.map(article => (
                 <div className="card" key={article.id}>
                   <img src={article.imagem} alt={article.titulo} className="card-image" /> 
-                  
                   <h2 id='title' className="card-title">{'"'+article.titulo+'"'}</h2>
                   <p id='p' className="card-desc">{article.desc}</p>
                   <br />
@@ -180,39 +186,39 @@ function Login() {
           <h1 id='login'>Login</h1>
           <br/>
           <div className="form">
-            
-          <form onSubmit={handleLogin}>
-          <p id='p'>Realize o Login p/ poder fazer a Leitura!</p>
-          <br/>
+            <form onSubmit={handleLogin}>
+              <p id='p'>Realize o Login p/ poder fazer a Leitura!</p>
+              <br/>
 
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Senha:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <br/>
-            <button type="submit">Entrar</button>
-            <br/>
-            <Link to="/cadastrar">
-            <button id="semConta">Não Tenho uma conta!</button>
-            </Link>
-            {message && <p className="message">{message}</p>}
-          </form>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Senha:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <br/>
+              <button type="submit">Entrar</button>
+              <br/>
+              <Link to="/cadastrar">
+                <button id="semConta">Não Tenho uma conta!</button>
+                <button id="semConta">Redefinir Senha</button>
+              </Link>
+              {message && <p className="message">{message}</p>}
+            </form>
           </div>
         </div>
       )}
